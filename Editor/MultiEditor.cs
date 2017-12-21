a0dfbc4bbc01e3997097a8ae5e2b95fb8b14eb36
Autofire <autofire555@gmail.com>
2017-12-21 13:02:57 -0800

    Added missing script.

---
+++ Editor/MultiEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MultiEditor : Editor {
+
+	private System.Collections.Generic.List<Object> targetList;
+
+	public MultiEditor() {
+		targetList = new System.Collections.Generic.List<Object>(1);
+	}
+
+	public virtual void OnMultiEdit(System.Collections.Generic.List<Object> targetList) {
+	}
+
+	public virtual void OnSceneGUI() {
+		if (targetList.Contains (target)) {
+			// We got all targets.
+			if (target == targetList [0]) {
+				// First call of OnSceneGUI(). Time to relay.
+				OnMultiEdit (targetList);
+			}
+		} else {
+			// Build the target list first, skipping the first event.
+			// First event usually is a Layout event which is generally not interesting anyway.
+			targetList.Add (target);
+		}
+	}
+}
